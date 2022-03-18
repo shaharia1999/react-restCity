ca0d2254eaac38cb2849b40071e36c057d05a860
Shaharia Ahmed <adeptshaharia@gmail.com>
2022-03-18 12:06:53 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {  useEffect, useState } from 'react';
+import City from './conpunant/City/City';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <City></City>
+      <LoadData></LoadData>
+      <Country></Country>
+      
+   
+      
+      
     </div>
   );
 }
-
+function LoadData(){
+  const [countries, setCountries] = useState([]);
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res=>res.json())
+    .then(data=>setCountries(data))
+  },[])
+return(
+  <div>
+    <h1> Rest countries With React App</h1>
+    <h2>Resut Countries:{countries.length}</h2>
+    {
+      countries.map(country=> <Country name ={country.name.common}flag={country.flags.png}></Country>)
+      
+    }
+  </div>
+)
+}
+function Country(props){
+  return(
+    <div style={{border: " 2px solid red",borderRadius:"20px",backgroundColor:"black" ,color:'white',marginTop:"20px"}}>
+      <h2> Name :{props.name}</h2>
+      <img src={props.flag} alt="" />
+    </div>
+  )
+}
 export default App;
---
+++ src/conpunant/City/City.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const City = () => {
+    return (
+        <div>
+            <h1>Haire Kopila Col Nakta te Uti</h1>
+            
+        </div>
+    );
+};
+
+export default City;
